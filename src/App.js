f1adad43c91dda876de13892c47154d087dcb868
Cervus23 <billiatrian23@gmail.com>
2021-03-02 18:10:57 +0200

    Remade banner slide (desktop & phone)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import reducers from './store/reducers';
 import Header from './containers/Header';
 // import Auth from './containers/Auth';
 import Footer from './containers/Footer';
+import { banner1, banner2 } from './img/index';
 import './App.scss';
 
 const store = createStore(reducers);
@@ -13,7 +14,11 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <div className="with-image"></div>
+        <div className="with-image">
+          <img className="banner" src={banner1} alt="banner1" />
+          <img className="banner" src={banner2} alt="banner2" />
+          <img className="banner" src={banner1} alt="banner1" />
+        </div>
         <Header width={width} />
         {width > 360 ? (
           <h1 className="main-title">Hello! Welcome to Aurora</h1>
